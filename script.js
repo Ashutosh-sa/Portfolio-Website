1295256e02ee69477c16cff041055fb2671af0e5
ashutosh yadav <ay502920@gmail.com>
2024-03-05 14:45:48 +0530

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["B.tech(computer science student)"],
+    strings: ["B.tech(computer science)"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
