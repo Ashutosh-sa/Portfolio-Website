d33b25a5a4196db792e4538124052f674fef6f57
ashutosh yadav <ay502920@gmail.com>
2024-07-09 20:46:17 +0530

    first

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["B.tech(computer science)"],
+    strings: ["B.tech(computer science student)"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true 
 })
+let header= document.querySelector("header")
+window.addEventListener("scroll",function(){
+   header.classList.toggle("sticky",window.scrollY>120);  
+}
+
+  )
